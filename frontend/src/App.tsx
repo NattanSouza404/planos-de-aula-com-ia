26ae161d16ab909c666f6816534964dd813eb186
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-22 02:20:43 -0300

    feat: avanços na tela principal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
-import { consultarTodosPlanosAula } from './service/api';
+import { adicionarAtividade, consultarTodosPlanosAula } from './service/api';
+import type { PlanoAula } from './model/PlanoAula';
+import { CartaoPlanoAula } from './components/CartaoPlanoAula';
+import type { PedidoCriacaoAula } from './model/PedidoCriacaoAula';
 
 function Page() {
 
-  const [planosAula, setPlanosAula] = useState<any[]>([]);
+  const [planosAula, setPlanosAula] = useState<PlanoAula[]>([]);
 
   useEffect(() => {
     async function getPlanosAula() {
@@ -17,47 +20,75 @@ function Page() {
     getPlanosAula();
   }, []);
 
+  const confirmarAdicionarAtividade = async (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    
+    if (!form){
+      return;
+    }
+    
+    const dadosFormulario = new FormData(form);
+
+    const promptUsado = dadosFormulario.get('promptUsado');
+    const etapaEnsinoBasico = dadosFormulario.get('etapaEnsinoBasico');
+
+    const pedidoAdicionarAula: PedidoCriacaoAula = {
+      promptUsado: promptUsado?.toString() || '',
+      etapaEnsinoBasico: etapaEnsinoBasico as 'EDUCACAO_INFANTIL' | 'ENSINO_FUNDAMENTAL' | 'ENSINO_MEDIO'
+    };
+    
+    const novoPlanoAula = await adicionarAtividade(pedidoAdicionarAula);
+
+    const novosPlanosAula = [...planosAula, novoPlanoAula];
+    setPlanosAula(novosPlanosAula);
+  }
+
   return (
-    <div>
+    <div id="container-app">
 
       <div>
         <h2>Gerador de planos de aula com IA</h2>
       </div>
 
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px' }}>
+      <form onSubmit={confirmarAdicionarAtividade} id="formulario-gerar-plano-de-aula">
         <label>Selecione a etapa da Educação Básica:</label>
-        <select>
+        <select name="etapaEnsinoBasico">
           <option value="EDUCACAO_INFANTIL">Educação Infantil</option>
           <option value="ENSINO_FUNDAMENTAL">Ensino Fundamental</option>
           <option value="ENSINO_MEDIO">Ensino Médio</option>
         </select>
         
         <label>Descreva o plano de aula que deseja gerar:</label>
-        <textarea placeholder='Gere uma aula de matemática sobre trigonometria'></textarea>
+        <textarea name='promptUsado' placeholder='Crie para mim uma aula de matemática introdutória sobre trigonometria, ensinando apenas os conceitos básicos.'></textarea>
 
-        <button>Gerar plano de aula</button>
+        <button type="submit">
+          Gerar plano de aula
+        </button>
+      </form>
+
+      {
+        planosAula.length > 0 ?
+
+        <div id="container-planos-aula">
+          {        
+              planosAula.map((plano, index) =>
+                <CartaoPlanoAula
+                  key={index}
+                  planoAula={plano}
+                />
+              )
+            }
+        </div>
+      :
+
+      <div>
+        <p>Sem aulas registradas</p>
       </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Descrição</th>
-          </tr>
-        </thead>
-        <tbody>
-          {planosAula.map((plano, index) => 
-            <tr key={index}>
-              <td>{plano.titulo}</td>
-              <td>{plano.descricao}</td>
-            </tr>
-          )}
-          <tr>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      }
+      
     </div>
   )
 }
